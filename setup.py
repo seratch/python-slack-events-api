e1bef55104abefbe6c3203b9f59a9e670b569111
Kazuhiro Sera <ksera@slack-corp.com>
2020-06-12 17:56:11 +0900

    Cleanup the package build
    * Remove unncessary requests, six from install_requires
    * Do tests with install_requires
    * Migrate to pyee 7 (EventEmitter is deprecated)
    * Migrate pytest to the latest
    * Add python 3.7, 3.8 to CI builds

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ def find_version(*file_paths):
                               version_file, re.M)
     if version_match:
         return version_match.group(1)
-    raise RuntimeError("Unable to find version string.")
+    raise RuntimeError('Unable to find version string.')
 
 setup(name='slackeventsapi',
       version=find_version('slackeventsapi', 'version.py'),
@@ -31,23 +31,24 @@ setup(name='slackeventsapi',
       license='MIT',
       packages=['slackeventsapi'],
       install_requires=[
-        'flask',
-        'pyee',
-        'requests',
-        'six',
+          'flask>=1,<2',
+          'pyee>=7,<8',
+          'itsdangerous<2', # for Python 2.7
+          'Jinja2<3', # for Python 2.7
+          'MarkupSafe<2', # for Python 2.7
       ],
       classifiers=[
-          "Development Status :: 5 - Production/Stable",
-          "Intended Audience :: Developers",
-          "Topic :: Communications :: Chat",
-          "Topic :: System :: Networking",
-          "Topic :: Office/Business",
-          "License :: OSI Approved :: MIT License",
-          "Programming Language :: Python",
-          "Programming Language :: Python :: 2.7",
-          "Programming Language :: Python :: 3.5",
-          "Programming Language :: Python :: 3.6",
-          "Programming Language :: Python :: 3.7",
-          "Programming Language :: Python :: 3.8",
+          'Development Status :: 5 - Production/Stable',
+          'Intended Audience :: Developers',
+          'Topic :: Communications :: Chat',
+          'Topic :: System :: Networking',
+          'Topic :: Office/Business',
+          'License :: OSI Approved :: MIT License',
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3.5',
+          'Programming Language :: Python :: 3.6',
+          'Programming Language :: Python :: 3.7',
+          'Programming Language :: Python :: 3.8',
       ],
       zip_safe=False)
